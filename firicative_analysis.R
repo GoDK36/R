1a3b72c2b65dc86363322bc174ae46fc97c73f14
Shin Woo Kim <45689846+GoDK36@users.noreply.github.com>
2019-11-25 11:58:18 +0900

    added dispersion

--- firicative_analysis.R
+++ firicative_analysis.R
@@ -7,8 +7,9 @@ s_formants
 c_s <- subset(s_formants, consonant=='s')
 c_ss <- subset(s_formants, consonant=='s*')
 dur <- duration ~ consonant
-cog <- log(cog) ~ consonant
+cog <- log(cog) ~ consonant #transform to log because too many outliers
 int <- intensity ~ consonant
+dis <- dispersion ~ consonant
 
 
 summary(c_s)
@@ -18,11 +19,13 @@ summary(c_ss)
 boxplot(dur,data = s_formants)
 boxplot(int,data = s_formants)
 boxplot(cog,data=s_formants)
+boxplot(dis,data = s_formants)
 
 ##T-tests
 t.test(dur, data = s_formants)
 t.test(int, data = s_formants)
 t.test(cog, data = s_formants)
+t.test(dis, data = s_formants)
 
-exp(7.263900)       
+exp(7.263900)   #change log to oridinary value    
 exp(6.336916)
