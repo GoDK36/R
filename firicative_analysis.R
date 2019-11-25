3f5f955b92dcecb48c91f392ea0c107d79b53efb
Shin Woo Kim <45689846+GoDK36@users.noreply.github.com>
2019-11-25 11:38:54 +0900

    Add files via upload

---
+++ firicative_analysis.R
@@ -0,0 +1,28 @@
+getwd()
+setwd("/home/s201500601123456/tutorial/tutorial7")
+
+s_formants = read.csv("s_s*.csv")
+s_formants
+
+c_s <- subset(s_formants, consonant=='s')
+c_ss <- subset(s_formants, consonant=='s*')
+dur <- duration ~ consonant
+cog <- log(cog) ~ consonant
+int <- intensity ~ consonant
+
+
+summary(c_s)
+summary(c_ss)
+
+##boxplots
+boxplot(dur,data = s_formants)
+boxplot(int,data = s_formants)
+boxplot(cog,data=s_formants)
+
+##T-tests
+t.test(dur, data = s_formants)
+t.test(int, data = s_formants)
+t.test(cog, data = s_formants)
+
+exp(7.263900)       
+exp(6.336916)
